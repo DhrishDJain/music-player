1e4e3aabf4d21d4e21649e46f92ca996d0e865aa
DhrishDJain <jaindhrish12@gmail.com>
2023-11-26 00:05:25 +0530

    changed image icon and added img to potion button

--- code.py
+++ code.py
@@ -5,8 +5,10 @@ import threading
 from time import sleep
 import pygame
 import os
+back_control_colour="#BEB89A"
+opbutt_color="#1D1D1D"
 root=tk.Tk()
-root.config(background="#dfdfdf")
+root.config(background=back_control_colour)
 root.minsize(width=700,height=500)
 pygame.mixer.init()
 #function to delete frame
@@ -14,22 +16,22 @@ def clear_frame(frame):
    frame.destroy()
 
 #side menu button frame
-opbuttf=Frame(root,background="#0AF7C2")
+opbuttf=Frame(root,background=opbutt_color)
 opbuttf.pack(side=LEFT,fill=Y)
 
 #icons
 plimg=PhotoImage(file="play.png")
+playlimg=PhotoImage(file="playlist.png")
+addimg=PhotoImage(file="add.png")
 opimg=PhotoImage(file="option-image.png")
+albimg=PhotoImage(file="music.png")
 previmg=PhotoImage(file="previous.png")
 pausimg=PhotoImage(file="pause.png")
 neximg=PhotoImage(file="next-button.png")
 
 #frames for progress bar and play/pause button
-control=Frame(root,background="#dfdfdf")
+control=Frame(root,background=back_control_colour)
 control.pack(side=BOTTOM,fill=X)
-playing=Frame(root,background="#dfdfdf")
-playing.pack(side=BOTTOM,fill=X)
-
 
 #intial width of progress bar
 global wid
@@ -107,16 +109,17 @@ def previoussong():
     global song_no
     song_no-=1
     load(song_no)
-previous_button=Button(control,width=24,border=0,background="#dfdfdf",activebackground="#dfdfdf",image=previmg,command=previoussong)
+
+previous_button=Button(control,width=32,border=0,background=back_control_colour,activebackground=back_control_colour,foreground=back_control_colour,image=previmg,command=previoussong)
 previous_button.pack(side=LEFT,padx=(270,0))
-play_button=Button(control,width=24,border=0,background="#dfdfdf",activebackground="#dfdfdf",image=plimg,command=music_starter)
+play_button=Button(control,width=32,border=0,background=back_control_colour,activebackground=back_control_colour,foreground=back_control_colour,image=plimg,command=music_starter)
 play_button.pack(side=LEFT,padx=(30,26))
-next_button=Button(control,width=24,border=0,background="#dfdfdf",activebackground="#dfdfdf",image=neximg,command=nextsong)
+next_button=Button(control,width=32,border=0,background=back_control_colour,activebackground=back_control_colour,foreground=back_control_colour,image=neximg,command=nextsong)
 next_button.pack(side=LEFT)
 
 #MUSIC STATUS PROGRESS BAR
-prog=Scale(playing,orient=HORIZONTAL,highlightthickness=0,width=4,length=660,foreground="#dfdfdf",background="red",troughcolor="red")
-prog.pack(side=LEFT,padx=6,fill=X)
+prog=Scale(root,orient=HORIZONTAL,highlightthickness=0,width=4,length=660,foreground="#dfdfdf",background=back_control_colour,troughcolor="red")
+prog.pack(side=BOTTOM,padx=6,fill=X)
 def load(index=0):
     global play_button
     global stop
@@ -146,12 +149,13 @@ def window_event(e=None):
     global prog
     scr=root.winfo_width()
     hig=root.winfo_height()
+    print(scr,hig)
     if scr==1366 and hig==705:
         prog.config(length=1366)
         previous_button.pack(side=LEFT,padx=(600,0))
         play_button.pack(side=LEFT,padx=(30,26))
         next_button.pack(side=LEFT)
-    if (scr==700 and hig==500) or (scr==800 and hig==500) or (scr==1370 and hig==500) or (scr==705 and hig==500) :
+    if (scr==700 and hig==500) or (scr==853 and hig==500) or (scr==1370 and hig==500) or (scr==782 and hig==500) :
         prog.config(length=660)
         previous_button.pack(side=LEFT,padx=(275,0))
         play_button.pack(side=LEFT,padx=(30,26))
@@ -163,69 +167,78 @@ def window_event(e=None):
 root.bind("<Map>", window_event)
 
 
-#function of side menu
-def side_menu():
-    global count#to check weather side bar is open or not
-    global option_frame#to pass side menu frame to clear frame function
-    global playing#to edit frame attributes out function once they are deleted for first time
-    global control#to edit frame attributes out function once they are deleted for first time
-    global prog#to keep progress bar in sync with when the menu is off/on 
-    global play_button#to edit button attributes out function once they are deleted for first time
-    global next_button#to edit button attributes out function once they are deleted for first time
-    global previous_button#to edit button attributes out function once they are deleted for first time
-    global stop
-    global stat
-    global wid
-    if count==0:
-        clear_frame(control)#to use fill=y of side menu
-        clear_frame(playing)#to use fill=y of side menu
-        option_frame=tk.Label(root,background="black")
-        option_frame.pack(side=LEFT,fill=Y)
-        #to increas size of window on opening side menu
-        if root.winfo_width()==700 and root.winfo_height()==500:
-            root.minsize(width=800,height=500)
+option_butt=Button(opbuttf,border=0,bg=opbutt_color,image=addimg,compound=tk.LEFT,width=70,text="      Add",foreground="white")
+option_butt.pack(pady=(45,11),padx=(0,12))
+option_butt=Button(opbuttf,border=0,bg=opbutt_color,image=albimg,compound=tk.LEFT,width=70,foreground="white",text="   Album")
+option_butt.pack(pady=15,padx=(0,12))
+option_butt=Button(opbuttf,border=0,bg=opbutt_color,height=30,image=playlimg,compound=tk.LEFT,width=70,text="   Playlist",foreground="white")
+option_butt.pack(pady=15,padx=(0,12))
+option_butt=Button(opbuttf,border=0,bg=opbutt_color,width=12,text="Podcast",foreground="white")
+option_butt.pack(pady=15,padx=(0,12))
 
-        #recreating/
-        control=Frame(root,background="#dedede") 
-        control.pack(side=BOTTOM,fill=X)
-        playing=Frame(root,background="#dedede") 
-        playing.pack(side=BOTTOM,fill=X)
+# #function of side menu
+# def side_menu():
+#     global count#to check weather side bar is open or not
+#     global option_frame#to pass side menu frame to clear frame function
+#     global playing#to edit frame attributes out function once they are deleted for first time
+#     global control#to edit frame attributes out function once they are deleted for first time
+#     global prog#to keep progress bar in sync with when the menu is off/on 
+#     global play_button#to edit button attributes out function once they are deleted for first time
+#     global next_button#to edit button attributes out function once they are deleted for first time
+#     global previous_button#to edit button attributes out function once they are deleted for first time
+#     global stop
+#     global stat
+#     global wid
+#     if count==0:
+#         clear_frame(control)#to use fill=y of side menu
+#         clear_frame(playing)#to use fill=y of side menu
+#         option_frame=tk.Label(root,background="black")
+#         option_frame.pack(side=LEFT,fill=Y)
+#         #to increas size of window on opening side menu
+#         if root.winfo_width()==700 and root.winfo_height()==500:
+#             root.minsize(width=800,height=500)
+
+#         #recreating/
+#         control=Frame(root,background="#dedede") 
+#         control.pack(side=BOTTOM,fill=X)
+#         playing=Frame(root,background="#dedede") 
+#         playing.pack(side=BOTTOM,fill=X)
         
-        prog=Scale(playing,orient=HORIZONTAL,highlightthickness=0,width=4,length=660,foreground="#dfdfdf",background="red",troughcolor="red")
-        prog.set(wid)
-        prog.pack(side=LEFT,padx=6)
-        previous_button=Button(control,width=24,border=0,background="#dfdfdf",activebackground="#dfdfdf",image=previmg,command=previoussong)
-        previous_button.pack(side=LEFT,padx=(270,0))
-        # if stat==False:
-        #     play_button=Button(control,width=24,border=0,background="#dfdfdf",activebackground="#dfdfdf",image=plimg,command=music_starter)
-        #     play_button.pack(side=LEFT,padx=(30,26))
-        if stop==True:
-            play_button=Button(control,width=24,border=0,background="#dfdfdf",activebackground="#dfdfdf",image=plimg,command=unpausesong)
-            play_button.pack(side=LEFT,padx=(30,26))
-        elif stop==False:
-            play_button=Button(control,width=24,border=0,background="#dfdfdf",activebackground="#dfdfdf",image=pausimg,command=pausesong)
-            play_button.pack(side=LEFT,padx=(30,26))
+#         prog=Scale(playing,orient=HORIZONTAL,highlightthickness=0,width=4,length=660,foreground="#dfdfdf",background="red",troughcolor="red")
+#         prog.set(wid)
+#         prog.pack(side=LEFT,padx=6)
+#         previous_button=Button(control,width=24,border=0,background="#dfdfdf",activebackground="#dfdfdf",image=previmg,command=previoussong)
+#         previous_button.pack(side=LEFT,padx=(270,0))
+#         # if stat==False:
+#         #     play_button=Button(control,width=24,border=0,background="#dfdfdf",activebackground="#dfdfdf",image=plimg,command=music_starter)
+#         #     play_button.pack(side=LEFT,padx=(30,26))
+#         if stop==True:
+#             play_button=Button(control,width=24,border=0,background="#dfdfdf",activebackground="#dfdfdf",image=plimg,command=unpausesong)
+#             play_button.pack(side=LEFT,padx=(30,26))
+#         elif stop==False:
+#             play_button=Button(control,width=24,border=0,background="#dfdfdf",activebackground="#dfdfdf",image=pausimg,command=pausesong)
+#             play_button.pack(side=LEFT,padx=(30,26))
 
-        next_button=Button(control,width=24,border=0,background="#dfdfdf",activebackground="#dfdfdf",image=neximg,command=nextsong)
-        next_button.pack(side=LEFT)
-        #\recreating
+#         next_button=Button(control,width=24,border=0,background="#dfdfdf",activebackground="#dfdfdf",image=neximg,command=nextsong)
+#         next_button.pack(side=LEFT)
+#         #\recreating
 
-        option_butt=Button(option_frame,border=0,bg="black",width=12,text="Add song",foreground="white")
-        option_butt.pack(pady=5,padx=9)
-        option_butt=Button(option_frame,border=0,bg="black",foreground="white",width=12,text="kk")
-        option_butt.pack(pady=5)
-        option_butt=Button(option_frame,border=0,bg="black",width=12,text="kk",foreground="white")
-        option_butt.pack(pady=5)
-        option_butt=Button(option_frame,border=0,bg="black",width=12,text="kk",foreground="white")
-        option_butt.pack(pady=5)
-        count+=1
-    else:
-        clear_frame(option_frame)
-        root.minsize(width=700,height=500)
-        count=0
+#         option_butt=Button(option_frame,border=0,bg="black",width=12,text="Add song",foreground="white")
+#         option_butt.pack(pady=5,padx=9)
+#         option_butt=Button(option_frame,border=0,bg="black",foreground="white",width=12,text="kk")
+#         option_butt.pack(pady=5)
+#         option_butt=Button(option_frame,border=0,bg="black",width=12,text="kk",foreground="white")
+#         option_butt.pack(pady=5)
+#         option_butt=Button(option_frame,border=0,bg="black",width=12,text="kk",foreground="white")
+#         option_butt.pack(pady=5)
+#         count+=1
+#     else:
+#         clear_frame(option_frame)
+#         root.minsize(width=700,height=500)
+#         count=0
  #SIDE MENU BUTTON  #0AF7C2      
-side_butt=Button(opbuttf,background="#0AF7C2",activebackground="#DEDEDE",image=opimg,width=29,border=0,command=side_menu)
-side_butt.pack(anchor=NW)
+# side_butt=Button(opbuttf,background="#0AF7C2",activebackground="#DEDEDE",image=opimg,width=29,border=0,command=side_menu)
+# side_butt.pack(anchor=NW)
 
 
 
